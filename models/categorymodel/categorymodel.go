c5808dafe698cf47f0a03df5b72088edb21e4e26
Areta Arwaa <aretaathayaa@gmail.com>
2024-11-27 18:09:06 +0700

    update: crud categories

--- models/categorymodel/categorymodel.go
+++ models/categorymodel/categorymodel.go
@@ -47,3 +47,34 @@ func Create(category entities.Category) bool {
 
 	return lastInsertId > 0
 }
+
+func Edit(id int) entities.Category {
+	row := config.DB.QueryRow(`SELECT id, name FROM categories WHERE id = ? `, id)
+
+	var category entities.Category
+
+	if err := row.Scan(&category.Id, &category.Name); err != nil {
+		panic(err.Error())
+	}
+
+	return category
+}
+
+func Update(id int, category entities.Category) bool {
+	query, err := config.DB.Exec(`UPDATE categories SET name = ?, updated_at = ? where id = ?`, category.Name, category.UpdatedAt, id)
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := query.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	return result > 0
+}
+
+func Delete(id int) error {
+	_, err := config.DB.Exec("DELETE FROM categories WHERE id = ?", id)
+	return err
+}
