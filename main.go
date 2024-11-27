d08f3e3b5abc2369ce49c8ecfd3a19cd2a992411
Areta Arwaa <aretaathayaa@gmail.com>
2024-11-27 21:02:56 +0700

    upload: product crud

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"go-web/config"
 	"go-web/controllers/categorycontroller"
 	"go-web/controllers/maincontroller"
+	"go-web/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -19,6 +20,13 @@ func main() {
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// 2. Products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
